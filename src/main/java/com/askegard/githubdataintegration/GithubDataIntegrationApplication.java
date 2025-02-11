8cb54b92e98729fc4410230aec237442ee97ca9b
gage-askegard <gage.askegard@gmail.com>
2025-02-10 21:25:31 -0600

    Add initial Spring Boot structure

---
+++ src/main/java/com/askegard/githubdataintegration/GithubDataIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.askegard.githubdataintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubDataIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubDataIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/askegard/githubdataintegration/GithubDataIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.askegard.githubdataintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubDataIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
